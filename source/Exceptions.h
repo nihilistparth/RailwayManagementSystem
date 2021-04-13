62910b2f69c02fddd5d029f4da8388d5942c7c7c
nihilist <nihilistcpp@gmail.com>
2021-04-13 15:11:12 +0530

    first commit

---
+++ source/Exceptions.h
@@ -0,0 +1,33 @@
+
+#ifndef __EXCEPTIONS_H
+#define __EXCEPTIONS_H
+
+#include<iostream>
+using namespace std;
+
+class Bad_Date: public exception{
+    public:
+        const char* what() const throw();
+};
+
+class Bad_Station: public exception{
+    public:
+        const char* what() const throw();
+};
+
+class Bad_Railways: public exception{
+    public:
+        const char* what() const throw();
+};
+
+class Bad_Booking: public exception{
+    public:
+        const char* what() const throw();
+};
+
+class Bad_Passenger: public exception{
+    public:
+        const char* what() const throw();
+};
+
+#endif
